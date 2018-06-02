1c067ac9950d29ad4f432abed0d5f4d5b07eede9
Dana F <danatania@gmail.com>
2018-06-02 15:28:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var a = Math.floor(Math.random() * 1000)
 var b = Math.floor(Math.random() * 1000)
   
 function add() {
-  return = 'a + b';
+  number += 'a + b';
 }
  
 add();
-console.log('a + b');
+console.log(number);
